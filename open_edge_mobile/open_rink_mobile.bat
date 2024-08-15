17c64f89a332004dbce98d723d5351001a5508c4
CharlieYang0040 <yhc1401@gmail.com>
2024-08-15 16:44:14 +0900

    open_rink_mobile upload

---
+++ open_edge_mobile/open_rink_mobile.bat
@@ -0,0 +1,2 @@
+@echo off
+start chrome --new-window --auto-open-devtools-for-tabs --user-agent="Mozilla/5.0 (iPad; CPU OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1" https://rink.kakaogames.com/
